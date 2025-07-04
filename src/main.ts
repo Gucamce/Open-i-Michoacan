d81c3abfe45d039a364f06ce1dfd26a7f8719098
gucamce <gucamce@gmail.com>
2025-07-04 03:16:47 -0600

    v1.1.8

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,3 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-/*   import { bootstrapApplication } from '@angular/platform-browser';
-  import { importProvidersFrom } from '@angular/core';
-  import { HttpClientModule } from '@angular/common/http';  // Importa HttpClientModule
-  import { appConfig } from './app/app.config';
-  import { AppComponent } from './app/app.component';
-  
-  bootstrapApplication(AppComponent, {
-    ...appConfig,
-    providers: [
-      importProvidersFrom(HttpClientModule)  // Agrega HttpClientModule aquí
-    ]
-  })
-    .catch((err) => console.error(err)); */
