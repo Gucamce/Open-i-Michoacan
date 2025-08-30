86e865674b6e590bdfcb890b8ed612b0b7dc52e1
gucamce <gucamce@gmail.com>
2025-08-29 21:13:40 -0600

    v0.1.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   mensaje: 'QA',
-  apiurl: 'https://repo.open-info.com.mx/' // Backend de producción real
+  apiurl: 'https://repo.open-info.com.mx' // Backend de producción real
 };
