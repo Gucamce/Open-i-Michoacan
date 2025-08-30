36657f6a87a999ca41513d55ef9d24703c9a68a6
gucamce <gucamce@gmail.com>
2025-08-29 20:31:28 -0600

    v0.1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-export const enviroment = {
-    production: true,
-    // apiurl: '/api'   // EC2 con Nginx
-    apiurl: 'http://prd-vum-alb-visor-1049821746.us-east-1.elb.amazonaws.com:8081/api' // Amplify
+export const environment = {
+  mensaje: 'Producción',
+  apiurl: 'https://prd-vum-alb-visor-1049821746.us-east-1.elb.amazonaws.com' // Backend de producción real
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   mensaje: 'Local',
-  // apiurl: 'http://ec2-18-189-182-146.us-east-2.compute.amazonaws.com:8086', //Desarrollo
+  apiurl: 'http://ec2-18-189-182-146.us-east-2.compute.amazonaws.com:8086', //Desarrollo
   // apiurl: 'http://prd-vum-alb-visor-1049821746.us-east-1.elb.amazonaws.com:80', //Produccion
-  apiurl: 'https://k8s-document-document-86f7800bb9-0020df151c32dbbd.elb.us-east-2.amazonaws.com' //Amplify
+  // apiurl: 'https://k8s-document-document-86f7800bb9-0020df151c32dbbd.elb.us-east-2.amazonaws.com' //Amplify
 }
